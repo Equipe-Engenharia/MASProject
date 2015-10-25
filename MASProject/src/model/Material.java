85bfbbc842c2f906e798032c7ffe5f7782e5b7d6
Fernando <koynonia@gmail.com>
2015-10-24 22:26:07 -0200

    Criado o model MATERIAL

---
+++ MASProject/src/model/Material.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Material {
+	
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
