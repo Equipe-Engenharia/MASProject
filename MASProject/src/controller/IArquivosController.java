3c17d7459085722c198673fd6f1e54f737c0227d
Fernando <koynonia@gmail.com>
2015-10-25 05:02:03 -0200

    Alterado o IArquivosController
    adicionado a possibilidade de transporte do texto dos campos para
    gravação no arquivo txt.

--- MASProject/src/controller/IArquivosController.java
+++ MASProject/src/controller/IArquivosController.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public interface IArquivosController {
 
 	public void leArquivo(String diretorio, String arquivo) throws IOException;
-	public void escreveArquivo(String diretorio, String arquivo, Object object) throws IOException;
+	public void escreveArquivo(String diretorio, String arquivo, String texto, Object object) throws IOException;
 	public void leDiretorio(String diretorio) throws IOException; 
 	
 }
