c5dd95447e0f4df9133a3e9ee8becfefe0dd58b6
Fernando <koynonia@gmail.com>
2015-10-25 05:07:13 -0200

    Alterado FormMaterial
    Implementado a gravação do novo registro.

--- MASProject/src/view/FormMaterial.java
+++ MASProject/src/view/FormMaterial.java
@@ -1,25 +1,31 @@
 package view;
 
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
-import controller.MaterialController;
 import javax.swing.JComboBox;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.awt.event.ActionEvent;
+
+import controller.MaterialController;
+import model.Material;
+import controller.ArquivosController;
+import controller.IArquivosController;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 public class FormMaterial extends JFrame {
 
- static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JComboBox<String> cbCategoria;
 	private JTextField txtMaterial;
+	Material material = new Material();
 
 	/**
 	 * Launch the application.
@@ -48,43 +54,56 @@ public class FormMaterial extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		cbCategoria = new JComboBox<String>();
 		cbCategoria.setBounds(172, 38, 178, 28);
 		contentPane.add(cbCategoria);
-		
+
 		txtMaterial = new JTextField();
+		txtMaterial.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				txtMaterial.setText(null);  //limpa a caixa de texto
+			}
+		});
+		txtMaterial.setToolTipText("Digite o novo material…");
 		txtMaterial.setText("Digite o novo material…");
 		txtMaterial.setBounds(172, 83, 178, 28);
 		contentPane.add(txtMaterial);
 		txtMaterial.setColumns(10);
-		
+
 		JLabel lblCategoria = new JLabel("Categoria da Obra");
 		lblCategoria.setBounds(47, 43, 113, 16);
 		contentPane.add(lblCategoria);
-		
+
 		JLabel lblMaterial = new JLabel("Material");
 		lblMaterial.setBounds(99, 89, 61, 16);
 		contentPane.add(lblMaterial);
-		
+
 		JButton btnGravar = new JButton("Gravar");
 		btnGravar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+
 				gravar();
-				
+
 			}
 		});
 		btnGravar.setBounds(369, 155, 117, 29);
 		contentPane.add(btnGravar);
-		
-		MaterialController listaCategoria = new MaterialController(cbCategoria); //Preechendo a comboBox CATEGORIA
+
+		MaterialController listaCategoria = new MaterialController(cbCategoria); // Preechendo a comboBox CATEGORIA
 		cbCategoria.addComponentListener(listaCategoria);
 	}
-	
 
-	
-	private void gravar(){ //Gravando o novo registro no arquivo.
-		
+	private void gravar() { 
+
+		IArquivosController arqContr = new ArquivosController();
+		material.setNome(txtMaterial.getText());
+		try {
+			arqContr.escreveArquivo("../MASProject/", "materiais", txtMaterial.getText(), material); // Gravando o novo registro no arquivo.
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
+	
 }
