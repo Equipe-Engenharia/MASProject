631e2778bf65a7349d0398c3385e060e91b6ec1c
Fernando <koynonia@gmail.com>
2015-10-25 23:10:28 -0200

    Material
    Atualizado com a implementação do Denys para tratar o campo informando
    ao usuário.
    Simplificada a ação de limpar o campo (dispensa o contador), usando uma
    propriedade do próprio campo "campo.setText(null)".
    Usada a função de limpar na gravação do campo para evitar que o usuário
    clique novamente em gravar e duplique a entrada.

--- MASProject/src/controller/MaterialController.java
+++ MASProject/src/controller/MaterialController.java
@@ -1,33 +1,116 @@
 package controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.IOException;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import model.Material;
 
 public class MaterialController implements ComponentListener{
 	
-	private JComboBox<String> cbCategoria;
+	private JLabel msgGravado, msgVazio;
+	private JComboBox<String> Categoria;
+	private JTextField nomeMaterial, idsetor;
+	private JButton btnGravar;
 	private ArquivosController arqController;
+	Material material = new Material();
 	
-	public MaterialController(JComboBox<String> cbCategoria){
-		this.cbCategoria = cbCategoria;
+	
+	public MaterialController(JComboBox<String> Categoria, JTextField id_setor, JTextField nomeDigit, JButton btnGravar, JLabel msgGravado, JLabel msgVazio) {
+		this.Categoria = Categoria;
+		this.idsetor = id_setor;
+		this.msgGravado = msgGravado;
+		this.msgVazio = msgVazio;
+		this.nomeMaterial = nomeDigit;
+		this.btnGravar = btnGravar;
 	}
+
 	
 	private void preencherComboBoxCategoria(){
 		String linha = new String();
+		
 		arqController = new ArquivosController();
 		try {
 			arqController.leArquivo("../MASProject/dados/", "categorias");
 			linha = arqController.getBuffer();
 			String [] categoria = linha.split(";");
 			for(String s : categoria){
-				cbCategoria.addItem(s);
+				Categoria.addItem(s);
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+	
+	public void gravaMaterial() { 
+
+		IArquivosController arqContr = new ArquivosController();
+		material.setNome(nomeMaterial.getText());
+		
+				if (!nomeMaterial.getText().isEmpty()) {  //se o campo não estiver vazio
+		try {
+			arqContr.escreveArquivo("../MASProject/dados/", "materiais", nomeMaterial.getText(), material); // Gravando o novo registro no arquivo.
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		msgGravado.setText(nomeMaterial.getText()+" salvo.");
+		msgGravado.setVisible(true);
+		nomeMaterial.setText(null);//limpa o campo previnindo gravar em duplicidade
+				}else{
+					msgGravado.setVisible(false);
+					msgVazio.setVisible(true);
+				}
+	}
+	
+	public ActionListener gravarMaterial = new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			gravaMaterial();
+		}
+	};
+
+	public MouseListener limpaCampo = new MouseListener() {
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			nomeMaterial.setText(null); //limpa o campo
+			msgGravado.setVisible(false); //para que a mensagem não fique visível a todo momento
+            msgVazio.setVisible(false);
+		}
+	};
 	
 	@Override
 	public void componentHidden(ComponentEvent e) {}
