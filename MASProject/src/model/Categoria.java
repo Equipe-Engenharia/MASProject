28be5995554a8498d45ee9ff72dc8043eaf1d4a3
Fernando <koynonia@gmail.com>
2015-10-26 00:05:14 -0200

    Retirado os erros de formatação nos caracteres acentuados. É preciso veirificar se é possível alterar para não acentuadas e efitar fururos erros.

--- MASProject/src/model/Categoria.java
+++ MASProject/src/model/Categoria.java
@@ -2,15 +2,15 @@ package model;
 
 public class Categoria {
 
-	private int identificação;
+	private int identificaÃ§Ã£o;
 	private String nome;
 	
-	public int getIdentificação() {
-		return identificação;
+	public int getIdentificaÃ§Ã£o() {
+		return identificaÃ§Ã£o;
 	}
 	
-	public void setIdentificação(int identificação) {
-		this.identificação = identificação;
+	public void setIdentificaÃ§Ã£o(int identificaÃ§Ã£o) {
+		this.identificaÃ§Ã£o = identificaÃ§Ã£o;
 	}
 	
 	public String getNome() {
