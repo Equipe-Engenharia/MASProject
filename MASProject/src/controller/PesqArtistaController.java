2eb16d790c8b2f998f9667eb421b2bd3b5c93532
Fernando <koynonia@gmail.com>
2015-10-29 14:05:01 -0200

    Pesquisa Artista:bug no preenchimento da comboBox

--- MASProject/src/controller/PesqArtistaController.java
+++ MASProject/src/controller/PesqArtistaController.java
@@ -30,14 +30,11 @@ public class PesqArtistaController{
 			arqController.leArquivo("../MASProject/dados", "artistas");
 			linha = arqController.getBuffer();
 			nArtista = linha.split(";");
-			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return nArtista;
 	}
-	
-	
 	
 	
 }
