c54fd125e9d103cbad13803844f389736097f5c5
Denys <denys.wyliam@gmail.com>
2015-12-01 14:08:55 -0200

    Resolvendo erros de envio

--- MASProject/src/controller/RelatorioFinCtrl.java
+++ MASProject/src/controller/RelatorioFinCtrl.java
@@ -1,5 +1,72 @@
 package controller;
 
+import java.awt.Color;
+import java.awt.GradientPaint;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.CategoryLabelPositions;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+
 public class RelatorioFinCtrl {
+	
+	public RelatorioFinCtrl(){
+		
+	}
+
+	public CategoryDataset criaDataset() {
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+		// TODO
+		return dataset;
+	}
+
+	public JFreeChart criaChart(CategoryDataset dataset) {
+		JFreeChart chart = ChartFactory.createBarChart("Titulo do grafico", "titulo eixo x", "titulo eixo y", dataset,
+				PlotOrientation.VERTICAL, // orientacao
+				true, // se quiser por legenda
+				true, // tooltips?
+				false // URLs?
+		);
+
+		// CUSTOMIZACOES
+
+		// seleciona o plano de fundo do grafico
+		chart.setBackgroundPaint(Color.white);
+
+		// pega uma referencia para customizacoes
+		CategoryPlot plot = chart.getCategoryPlot();
+		plot.setBackgroundPaint(Color.lightGray);
+		plot.setDomainGridlinePaint(Color.white);
+		plot.setRangeGridlinePaint(Color.white);
+
+		// seleciona o eixo para mostrar apenas inteiros
+		NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+		rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+
+		// Desativa sublinhas da barra
+		BarRenderer renderer = (BarRenderer) plot.getRenderer();
+		renderer.setDrawBarOutline(false);
+
+		// Coloca cores gradientes nas barras
+		GradientPaint gp0 = new GradientPaint(0.0f, 0.0f, Color.blue, 0.0f, 0.0f, Color.lightGray);
+		GradientPaint gp1 = new GradientPaint(0.0f, 0.0f, Color.green, 0.0f, 0.0f, Color.lightGray);
+		GradientPaint gp2 = new GradientPaint(0.0f, 0.0f, Color.red, 0.0f, 0.0f, Color.lightGray);
+		renderer.setSeriesPaint(0, gp0);
+		renderer.setSeriesPaint(1, gp1);
+		renderer.setSeriesPaint(2, gp2);
+
+		CategoryAxis domainAxis = plot.getDomainAxis();
+		domainAxis.setCategoryLabelPositions(CategoryLabelPositions.createUpRotationLabelPositions(Math.PI / 6.0));
+		// Customizacao termina aqui
+
+		return chart;
+
+	}
 
 }
--- MASProject/src/view/FrmRelatorioFinanceiro.java
+++ MASProject/src/view/FrmRelatorioFinanceiro.java
@@ -1,32 +1,45 @@
 package view;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.data.category.CategoryDataset;
+
+import controller.RelatorioFinCtrl;
+
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JSeparator;
 import javax.swing.ImageIcon;
+import javax.swing.JComboBox;
+import javax.swing.JInternalFrame;
+import javax.swing.border.MatteBorder;
+import java.awt.Color;
 
 public class FrmRelatorioFinanceiro extends JFrame {
 
 	private JPanel contentPane;
+	private static final long serialVersionUID = 1L;
+
+	private JInternalFrame internalFrameGrafico;
 	private CategoryDataset dataset;
-	//= createDataset();
-	private  JFreeChart chart;
-	//createChart(dataset);
+	private JFreeChart chart;
 	private ChartPanel chartPanel = new ChartPanel(chart);
 	private JTextField txtDataIni;
 	private JTextField txtDataFim;
+	private JTextField txtGanho;
+	private JTextField textField;
+	private JLabel lblPeriodo, lblDataInicial, lblDataFinal, lblSelecioneOQue, lblCategoria, lblSubcategoria,
+			lblInformaesGerais, lblGanhor, lblDespesas;
+	private JButton btnSalvarimprimir, btnDataIni, btnDataFim;
+	private JSeparator separator, separator_1, separator_2;
+	// arrumar argumento
+	private JComboBox<Object> cbCategoria, cbSubCategoria;
 
 	/**
 	 * Launch the application.
@@ -48,54 +61,123 @@ public class FrmRelatorioFinanceiro extends JFrame {
 	 * Create the frame.
 	 */
 	public FrmRelatorioFinanceiro() {
+		setTitle("Relatório Financeiro");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 613, 459);
+		setBounds(100, 100, 789, 546);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		JLabel lblPerodo = new JLabel("Período:");
-		lblPerodo.setBounds(10, 11, 60, 14);
-		contentPane.add(lblPerodo);
-		
-		JLabel lblDataInicial = new JLabel("Data Inicial:");
-		lblDataInicial.setBounds(10, 46, 72, 14);
+
+		lblPeriodo = new JLabel("Período:");
+		lblPeriodo.setBounds(41, 12, 60, 14);
+		contentPane.add(lblPeriodo);
+
+		lblDataInicial = new JLabel("Data Inicial");
+		lblDataInicial.setBounds(41, 47, 72, 14);
 		contentPane.add(lblDataInicial);
-		
+
 		txtDataIni = new JTextField();
-		txtDataIni.setBounds(92, 43, 86, 20);
+		txtDataIni.setBounds(123, 44, 86, 20);
 		contentPane.add(txtDataIni);
 		txtDataIni.setColumns(10);
-		
-		JLabel lblDataFinal = new JLabel("Data Final:");
-		lblDataFinal.setBounds(246, 46, 72, 14);
+
+		lblDataFinal = new JLabel("Data Final");
+		lblDataFinal.setBounds(381, 47, 72, 14);
 		contentPane.add(lblDataFinal);
-		
+
 		txtDataFim = new JTextField();
-		txtDataFim.setBounds(328, 43, 86, 20);
+		txtDataFim.setBounds(463, 44, 86, 20);
 		contentPane.add(txtDataFim);
 		txtDataFim.setColumns(10);
-		
-		JButton btnDataIni = new JButton("");
-		btnDataIni.setIcon(new ImageIcon(FrmRelatorioFinanceiro.class.getResource("/com/toedter/calendar/images/JDateChooserColor32.gif")));
-		btnDataIni.setBounds(188, 42, 29, 23);
+
+		btnDataIni = new JButton("");
+		btnDataIni.setIcon(new ImageIcon(
+				FrmRelatorioFinanceiro.class.getResource("/com/toedter/calendar/images/JDateChooserColor32.gif")));
+		btnDataIni.setBounds(219, 43, 29, 23);
 		contentPane.add(btnDataIni);
-		
-		JButton btnDataFim = new JButton("");
-		btnDataFim.setIcon(new ImageIcon(FrmRelatorioFinanceiro.class.getResource("/com/toedter/calendar/images/JDateChooserColor32.gif")));
-		btnDataFim.setBounds(424, 42, 29, 23);
+
+		btnDataFim = new JButton("");
+		btnDataFim.setIcon(new ImageIcon(
+				FrmRelatorioFinanceiro.class.getResource("/com/toedter/calendar/images/JDateChooserColor32.gif")));
+		btnDataFim.setBounds(559, 43, 29, 23);
 		contentPane.add(btnDataFim);
-		
-		JLabel lblSelecioneOQue = new JLabel("Selecione o que você deseja visualizar:");
-		lblSelecioneOQue.setBounds(10, 86, 207, 14);
+
+		lblSelecioneOQue = new JLabel("Selecione o que você deseja visualizar:");
+		lblSelecioneOQue.setBounds(41, 86, 207, 14);
 		contentPane.add(lblSelecioneOQue);
-		
-		JSeparator separator = new JSeparator();
-		separator.setBounds(0, 71, 597, 4);
+
+		separator = new JSeparator();
+		separator.setBounds(0, 71, 773, 4);
 		contentPane.add(separator);
-//		chartPanel.setPreferredSize(new Dimension(500, 270));
-//		setContentPane(chartPanel);
-//		chartPanel.setLayout(null);
+
+		lblCategoria = new JLabel("Categoria");
+		lblCategoria.setBounds(41, 119, 60, 14);
+		contentPane.add(lblCategoria);
+
+		// arrumar argumentos das combos
+		cbCategoria = new JComboBox<Object>();
+		cbCategoria.setBounds(123, 116, 125, 20);
+		contentPane.add(cbCategoria);
+
+		lblSubcategoria = new JLabel("Sub-Categoria");
+		lblSubcategoria.setBounds(356, 116, 86, 14);
+		contentPane.add(lblSubcategoria);
+
+		// Arrumar argumentos das combos
+		cbSubCategoria = new JComboBox<Object>();
+		cbSubCategoria.setBounds(463, 113, 125, 20);
+		contentPane.add(cbSubCategoria);
+
+		separator_1 = new JSeparator();
+		separator_1.setBounds(0, 155, 773, 4);
+		contentPane.add(separator_1);
+
+		internalFrameGrafico = new JInternalFrame("Gráfico");
+		internalFrameGrafico.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		internalFrameGrafico.setFrameIcon(null);
+		internalFrameGrafico.setBounds(10, 170, 438, 267);
+		contentPane.add(internalFrameGrafico);
+		internalFrameGrafico.setVisible(true);
+
+		// chartPanel.setPreferredSize(new Dimension(500, 270));
+		internalFrameGrafico.setContentPane(chartPanel);
+		// setContentPane(chartPanel);
+		chartPanel.setLayout(null);
+
+		lblInformaesGerais = new JLabel("Informações Gerais:");
+		lblInformaesGerais.setBounds(477, 171, 130, 14);
+		contentPane.add(lblInformaesGerais);
+
+		lblGanhor = new JLabel("Ganho(R$)");
+		lblGanhor.setBounds(531, 209, 72, 14);
+		contentPane.add(lblGanhor);
+
+		txtGanho = new JTextField();
+		txtGanho.setBounds(613, 206, 86, 20);
+		contentPane.add(txtGanho);
+		txtGanho.setColumns(10);
+
+		lblDespesas = new JLabel("Despesas(R$)");
+		lblDespesas.setBounds(531, 250, 72, 14);
+		contentPane.add(lblDespesas);
+
+		textField = new JTextField();
+		textField.setBounds(613, 247, 86, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+
+		btnSalvarimprimir = new JButton("Salvar/Imprimir");
+		btnSalvarimprimir.setIcon(new ImageIcon("../MASProject/icons/save.png"));
+		btnSalvarimprimir.setBounds(608, 468, 155, 29);
+		contentPane.add(btnSalvarimprimir);
+
+		separator_2 = new JSeparator();
+		separator_2.setBounds(0, 453, 773, 4);
+		contentPane.add(separator_2);
+
+		RelatorioFinCtrl relaFinCtrl = new RelatorioFinCtrl();
+		dataset = relaFinCtrl.criaDataset();
+		chart = relaFinCtrl.criaChart(dataset);
+
 	}
 }
