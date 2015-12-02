2d5955ce655d93f1119d10dd0ca805ebd132568b
Alison05 <alison_carlos05@hotmail.com>
2015-12-02 17:53:38 -0200

    Mandando o combobox pro construtor

--- MASProject/src/controller/RelatorioEstCtrl.java
+++ MASProject/src/controller/RelatorioEstCtrl.java
@@ -1,6 +1,9 @@
 package controller;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JInternalFrame;
+import javax.swing.JTextField;
 
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
@@ -9,11 +12,23 @@ public class RelatorioEstCtrl {
 	private JFreeChart chart;
 	private ChartPanel chartPanel;
 	private JInternalFrame internalFrameGrafico;
+	private JComboBox<String> cbFiltro;
+	private JTextField txtDataIni, txtDataFim;
+	private JButton btnGerar, btnSalvar;
 
-	public RelatorioEstCtrl(JFreeChart chart, ChartPanel chartPanel, JInternalFrame internalFrameGrafico) {
+	public RelatorioEstCtrl(JFreeChart chart, ChartPanel chartPanel, JInternalFrame internalFrameGrafico,
+			JTextField txtDataIni, JTextField txtDataFim, JComboBox<String> cbFiltro, 
+			JButton btnGerar, JButton btnSalvarimprimir) {
+		
 		this.internalFrameGrafico = internalFrameGrafico;
 		this.chart = chart;
 		this.chartPanel = chartPanel;
+		this.btnGerar= btnGerar;
+		this.btnSalvar= btnSalvarimprimir;
+		this.txtDataFim = txtDataFim;
+		this.txtDataIni= txtDataIni;
+		this.cbFiltro= cbFiltro;
+	
 	}
 	
 	
